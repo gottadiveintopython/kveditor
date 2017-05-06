641a571a7a208c67e7fed8d296c45a2e5adcc9a1
GottaDiveIntoPython <flow4re2c@gmail.com>
2017-05-06 16:57:02 +0900

    Rename 'Update' to 'Preview'

--- kveditor/main.py
+++ kveditor/main.py
@@ -34,13 +34,13 @@ class KvEditor(Factory.FloatLayout):
         r'''Keyboard入力があった時に呼ばれるMethod
 
         以下のShortcutKeyを実現している
-            Ctrl + U => Update
+            Ctrl + P => Preview
             Ctrl + S => Save
             Ctrl + L => Load
         '''
         if len(modifiers) == 1 and modifiers[0] == r'ctrl':
-            if codepoint == r'u':
-                self.kve_update()
+            if codepoint == r'p':
+                self.kve_preview()
             elif codepoint == r's':
                 self.kve_save()
             elif codepoint == r'l':
@@ -70,34 +70,36 @@ class KvEditor(Factory.FloatLayout):
     def kve_save(self):
         r'''EditorのtextをFileに書き込む'''
         editor = self.ids.editor
-        text = tab2spaces(editor.text)
-        editor.text = text
+        editor.text = tab2spaces(editor.text)
         filepath = self.ids.ti_filepath.text
         try:
             with io.open(filepath, r'wt', encoding=r'utf-8') as writer:
-                writer.write(text)
+                writer.write(editor.text)
         except (OSError, IOError) as e:
             show_message_popup("File: '{}'\n{}".format(filepath, e.strerror))
 
-    def kve_update(self):
+    def kve_preview(self):
         r'''EditorのtextからWidgetを作って左側にPreview
 
         Builder.load_stringは
         1. 与えられたKvコードに問題があると例外を投げる
         2. 与えられたKvコードにroot ruleが無い時はNoneを返す
-        ようなので、その場合はそれがユーザーに分かるようエラーメッセージの書かれたLabelを作って貼り付けている
+        ようなので、その場合はそれがユーザーに分かるようエラーメッセージの書かれた
+        Labelを作って貼り付けている
         '''
 
         editor = self.ids.editor  # Editor部分のWidget(CodeInput)
         preview = self.ids.preview  # Preview貼り付け先のWidget
-        text = tab2spaces(editor.text)  # ユーザーがEditorに入力したコード
-        editor.text = text
+        editor.text = tab2spaces(editor.text)
         preview.clear_widgets()  # 以前のPreviewを破棄
         Builder.unload_file(self.KV_FILENAME)  # 以前のKvコードを無効化
 
         instance = None
         try:
-            instance = Builder.load_string(text, filename=self.KV_FILENAME)
+            instance = Builder.load_string(
+                editor.text,
+                filename=self.KV_FILENAME
+            )
         except Exception as e:
             temp = [str(e.__class__)]
             temp.extend([str(arg) for arg in e.args])
@@ -106,7 +108,8 @@ class KvEditor(Factory.FloatLayout):
             if instance is None:
                 error_msg = r'No root rules.'
         if instance is None:
-            # Widgetの作成に失敗した時は、代わりにエラーメッセージの書かれたLabelを貼り付ける
+            # Widgetの作成に失敗した時は、代わりにエラーメッセージの書かれたLabel
+            # を貼り付ける
             preview.add_widget(Factory.Label(
                 size_hint=(1, 1,),
                 text=error_msg))
@@ -114,24 +117,19 @@ class KvEditor(Factory.FloatLayout):
             preview.add_widget(instance)
 
 
-class StencilTouch(Factory.Widget):
-    pass
+class Preview(Factory.StencilView, Factory.RelativeLayout):
 
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            return super(Preview, self).on_touch_down(touch)
 
-def _override_on_touch_xxx_methods(cls):
-    for name in r'up move down'.split():
-        method_name = r'on_touch_' + name
+    def on_touch_move(self, touch):
+        if self.collide_point(*touch.pos):
+            return super(Preview, self).on_touch_move(touch)
 
-        def on_touch_xxx(self, touch, _method_name=method_name):
-            if self.collide_point(*touch.pos):
-                return getattr(super(cls, self), _method_name)(touch)
-            else:
-                return False
-
-        setattr(cls, method_name, on_touch_xxx)
-
-
-_override_on_touch_xxx_methods(StencilTouch)
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            return super(Preview, self).on_touch_up(touch)
 
 
 if __name__ == r'__main__':
